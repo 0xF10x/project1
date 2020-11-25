fb933558494d7ebb555226e36a71ebb9c804da4e
NSFW-Loli <444777222ak@gmail.com>
2020-11-26 00:19:11 +0300

    script changes
    script changes, added new functions and features

---
+++ Assets/Scripts/tap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class tap : MonoBehaviour, IPointerClickHandler 
+{
+	public bool k = false;
+	
+	[SerializeField] private List<Item> _Items;
+
+	public void OnPointerClick(PointerEventData eventData){
+    	
+    	if (k == false) 
+    	{
+    	Instantiate(_Items[Random.Range(0, _Items.Count)], transform.position, Quaternion.identity);
+    	k = true;
+    	}
+    }
+}
